c0b4aff3fa03c008b894e3762e16442f3b889ec5
junwen <junwen182@gmail.com>
2015-12-08 01:14:23 +0800

    fixed search and column filters ers bugs

--- js/app.js
+++ js/app.js
@@ -10,32 +10,27 @@ $(function() {
   $( "#end-date" ).datepicker();
 });
 
-// Blue highlight on filter options
-
-  // function that calls on an event: when a filter category is toggled open
-    // scan all the filter categories
-      // if it doesn't have the class ___ (i.e. its active)
-        // change its css: remove display: none on the blue highlight
-
-
 // Extended Department/Agency Filters
-
 $(".all-agencies").click(function(e) {
   e.preventDefault();
   if ( !$(".filter-agency").hasClass("extended-filter-appear") ) {
     $(".filter-agency").toggleClass("extended-filter-appear");
   }
 });
-
 $(".all-offices").click(function(e) {
   e.preventDefault();
   if ( !$(".filter-office").hasClass("extended-filter-appear") ) {
     $(".filter-office").toggleClass("extended-filter-appear");
   }
 });
-
 $(".close-box").click(function(e) {
   e.preventDefault();
   $(this).closest(".extended-filter").toggleClass("extended-filter-appear");
+});
+
+// Prevent "Managing Columns" drop-down menu from closing when clicked anywhere besides the header
+$(".table-options-show-columns .dropdown-menu").click(function (e)
+{
+    e.stopPropagation();
 });
 
