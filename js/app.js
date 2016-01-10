7c36873d41e0814b685727c9035f2e7f5cbb1bcf
junwen <junwen182@gmail.com>
2016-01-11 01:27:48 +0800

    Adding filter pages & fixing bugs

--- js/app.js
+++ js/app.js
@@ -34,3 +34,16 @@ $(".table-options-show-columns .dropdown-menu").click(function (e)
     e.stopPropagation();
 });
 
+// Slider picker
+$(function() {
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [ 75, 300 ]
+  });
+});
+
+$(function() {
+  $( "#slider" ).slider();
+});
